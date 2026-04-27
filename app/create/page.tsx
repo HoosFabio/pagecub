c9897bc16239e8ebef7f968f8a0b2a4f0b293a5d
HoosFabio <fabian@inksynth.org>
2026-04-27 11:20:51 -0400

    Add PageCub frontend scaffold

---
+++ app/create/page.tsx
@@ -0,0 +1,9 @@
+import { CreateWizard } from "@/components/CreateWizard";
+
+export default function CreatePage() {
+  return (
+    <main className="page-shell py-12">
+      <CreateWizard />
+    </main>
+  );
+}
